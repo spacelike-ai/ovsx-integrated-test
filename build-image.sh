3ba20ae85d4b6a7bdcff774634bb8a6c936d3dd4
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2024-10-21 10:36:52 +0200

    fix

--- build-image.sh
+++ build-image.sh
@@ -16,16 +16,13 @@ git clone --depth=1 --branch=devspaces-$DEVSPACES_VERSION-rhel-8 \
 
 extension_file="$tempdir/dependencies/che-plugin-registry/openvsx-sync.json"
 originial_extension_file="$tempdir/orig.json"
-new_extension_file="additional-extensions.json"
+additional_extension_file="additional-extensions.json"
 
 mv "$extension_file" "$originial_extension_file"
-jq -s '.[0] + .[1]' "$extension_file" "$new_extension_file" >"$extension_file"
+jq -s '.[0] + .[1]' "$originial_extension_file" "$additional_extension_file" >"$extension_file"
 
 (
-    # Suppress following question from `npx`:
-    #   Need to install the following packages:
-    #   @eclipse-che/plugin-registry-generator@7.92.0-dev-0a45f43
-    #   Ok to proceed?
+    # Suppress following question from `npx`: Need to install the following packages: [..] Ok to proceed?
     export npm_config_yes=true
 
     # Explicitly set the builder, to make sure that the build script uses the correct builder.
